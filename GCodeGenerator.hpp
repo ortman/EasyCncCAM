aebf9f12f30b526dd2ed2d5cec5582ac433131a0
sergeyroots <sergey_root@inbox.ru>
2024-12-15 13:38:34 +0500

    Created GCodeGenerator class

---
+++ GCodeGenerator.hpp
@@ -0,0 +1,14 @@
+#ifndef __GCODE_GENERATOR__
+#define __GCODE_GENERATOR__
+
+#include <Core/Core.h>
+
+using namespace Upp;
+
+class GCodeGenerator {
+public:
+	static StringStream Generate(const Vector<Operation*> &operations) {
+	}
+};
+
+#endif
